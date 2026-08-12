6ea14828ea27dd5410b574bb4f81e7a09b6d6bb2
OSRosr-0115 <junhio0115@outlook.jp>
2026-08-12 09:57:48 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: '-osr-business-portal/',
+    base: '/osr-business-portal/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
